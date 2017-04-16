081dab23b4eb2cef9808fa634adbdeaaeb11f654
tophski <toph.kinsey@gmail.com>
2017-04-15 18:01:45 -0600

    More work.

--- js/routes.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Chris on 4/11/2017.
- */
-module.exports = function(app){
-    app.get('/submitpdf/:path', function(req, res){
-        console.log(req.params['path']);
-    });
-}
