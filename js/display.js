47bbe8cfc5205d4a45db5e1535332d5072b6d30e
Kaleb Bickmore <kbic17@gmail.com>
2017-04-25 17:21:06 -0600

    added display.js

---
+++ js/display.js
@@ -0,0 +1,9 @@
+let $ = require('jquery');
+ipcRender = require('electron').ipcRenderer;
+ipc.on('timelineData', function(event, arg){
+  $('#table tr').not(':first').not(':last').remove();
+var html = '';
+for(var i = 0; i < arg.length; i++)
+            html += '<tr><td>[' + arg[i].date + ']</td><td>' + arg[i].sentence+ '</td></tr>'+ "[Page: "+arg[i].page+ ']</td></tr>';
+$('#thetable tr').first().after(html);
+}
